ec9010dd0c8c280b6f9167b739fb633e86940933
ashah174 <147004478+ashah174@users.noreply.github.com>
2026-06-20 14:05:16 -0700

    Change Firebase config to new project settings
    Updated Firebase configuration with new credentials.

--- TravelEz/src/firebase.js
+++ TravelEz/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpDyTWD9FOEIAIO5VtLxWMPLzdG93YFSk",
-  authDomain: "travelez-1292b.firebaseapp.com",
-  projectId: "travelez-1292b",
-  storageBucket: "travelez-1292b.firebasestorage.app",
-  messagingSenderId: "10760241629959",
-  appId: "1:10760241629959:web:ebcfc27088193df9c14efa",
-  measurementId: "G-KCMMDWRHLL",
+  apiKey: "AIzaSyAWVakx68ACosbA32l0ql_NDrWY2Sj3B94",
+  authDomain: "travelez-9c5ba.firebaseapp.com",
+  projectId: "travelez-9c5ba",
+  storageBucket: "travelez-9c5ba.firebasestorage.app",
+  messagingSenderId: "529377496796",
+  appId: "1:529377496796:web:fa6563ad700e0c308cee19",
+  measurementId: "G-359BQP1FXK"
 };
 
 const app = initializeApp(firebaseConfig);
