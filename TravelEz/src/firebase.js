e82d2a49bb471e7f742765520109d7f55dfeb80b
ashah174 <147004478+ashah174@users.noreply.github.com>
2026-06-20 14:23:00 -0700

    Remove analytics initialization
    Removed analytics initialization from firebase.js

--- TravelEz/src/firebase.js
+++ TravelEz/src/firebase.js
@@ -12,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
